80f63299195104975a8586170841811ba08dcd43
orumiya <eszterterka@gmail.com>
2020-09-24 20:06:17 +0200

    module example

---
+++ MainModule/src/runPackage/RunBaby.java
@@ -0,0 +1,10 @@
+package runPackage;
+
+import mainpackage.TestMainModuleClass;
+
+public class RunBaby {
+    public static void main(String[] args) {
+
+        TestMainModuleClass.display();
+    }
+}
---
+++ TestModule/src/mainpackage/TestMainModuleClass.java
@@ -0,0 +1,11 @@
+package mainpackage;
+
+public class TestMainModuleClass {
+    public static void main(String[] args) {
+        display();
+    }
+
+    public static void display() {
+        System.out.println("this comes from the modul");
+    }
+}
